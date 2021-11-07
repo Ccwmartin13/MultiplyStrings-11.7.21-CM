8023d0f002fc388e83be3d0e93d3784d34f42bca
Chase Martin <ccwmartin13@gmail.com>
2021-11-07 12:16:25 -0600

    Refactored Code, and added error handling

--- Program.cs
+++ Program.cs
@@ -9,21 +9,39 @@ namespace TestApp3_11._7._21_CM
         {
             Console.WriteLine("Please enter the first number: ");
             string firstNumber = Console.ReadLine();
+
             Console.WriteLine("Please enter the second number: ");
             string secondNumber = Console.ReadLine();
 
-            Console.WriteLine(firstNumber + " * " + secondNumber + " = " + Multiply(firstNumber, secondNumber));
+            try
+            {
+                Console.WriteLine(firstNumber + " * " + secondNumber + " = " + Multiply(firstNumber, secondNumber));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             Console.ReadKey();
         }
 
         public static string Multiply(string num1, string num2)
         {
-            BigInteger firstNumber = BigInteger.Parse(num1);
-            BigInteger secondNumber = BigInteger.Parse(num2);
+            BigInteger firstNumber = TryParseBigInt(num1);
 
-            BigInteger finalNumber = (firstNumber * secondNumber);
+            BigInteger secondNumber = TryParseBigInt(num2);
 
-            return finalNumber.ToString();
+            return (firstNumber * secondNumber).ToString();
+        }
+
+        private static BigInteger TryParseBigInt(string possibleNumber)
+        {
+            if (!BigInteger.TryParse(possibleNumber, out BigInteger actualNumber))
+            {
+                throw new Exception($"Attempted conversion of '{possibleNumber ?? " < null > "}' failed.");
+            }
+
+            return actualNumber;
         }
     }
 }
