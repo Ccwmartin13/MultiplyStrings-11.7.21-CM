1bc4fd154c0a466c8086a23ef05e3b65d84b4b89
Chase Martin <ccwmartin13@gmail.com>
2021-11-07 11:50:49 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace TestApp3_11._7._21_CM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number: ");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Please enter the second number: ");
+            string secondNumber = Console.ReadLine();
+
+            Console.WriteLine(firstNumber + " * " + secondNumber + " = " + Multiply(firstNumber, secondNumber));
+            Console.ReadKey();
+        }
+
+        public static string Multiply(string num1, string num2)
+        {
+            BigInteger firstNumber = BigInteger.Parse(num1);
+            BigInteger secondNumber = BigInteger.Parse(num2);
+
+            BigInteger finalNumber = (firstNumber * secondNumber);
+
+            return finalNumber.ToString();
+        }
+    }
+}
